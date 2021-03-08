309c17fb3deb5a68371ef3640083bf3cc6459ca9
tsadamori <tomoaki.sadamori@gmail.com>
2021-03-08 13:55:43 +0900

    save name by enter key

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -50,6 +50,7 @@ export default function SignIn({ setName }) {
   const classes = useStyles();
   const [disabled, setDisabled] = useState(true);
   const [string, setString] = useState('');
+  const [isComposed, setIsComposed] = useState(false);
   console.log(string);
 
   useEffect(() => {
@@ -76,11 +77,15 @@ export default function SignIn({ setName }) {
             autoFocus
             onChange={(e) => setString(e.target.value)}
             onKeyDown={(e) => {
+              if (isComposed) return;
+
               if (e.key === 'Enter') {
-                setName(e.target.value);
+                // setName(e.target.value);
                 e.preventDefault();
               }
             }}
+            onCompositionStart={() => setIsComposed(true)}
+            onCompositionEnd={() => setIsComposed(false)}
           />
           <Button
             type="button"
