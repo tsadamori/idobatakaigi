15621d4283b220fd1c791004df92f06787de44bb
tsadamori <tomoaki.sadamori@gmail.com>
2021-03-12 08:02:25 +0900

    adjust focusing

--- src/components/MessageSubmitButton.js
+++ src/components/MessageSubmitButton.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { IconButton } from '@material-ui/core';
+import { IconButton, InputLabel } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 
 import { pushMessage } from '../firebase';
 
-const MessageSubmitButton = ({ name, setText, text }) => {
+const MessageSubmitButton = ({ inputEl, name, setText, text }) => {
     return (
         <IconButton
             disabled={text === ''}
             onClick={() => {
                 pushMessage({ name, text });
                 setText('');
+                inputEl.current.focus();
             }}>
             <SendIcon />
         </IconButton>
